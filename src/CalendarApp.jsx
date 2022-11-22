6a2a07da6b07c7abdd9f2e28fc1205c46d046d87
Gu3r0 <jaimerodriguez123456@gmail.com>
2022-11-21 23:40:55 -0400

    Version 0.0.7

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -1,14 +1,16 @@
 import { AppRouter } from "./router";
-import { BrowserRouter } from "react-router-dom";
-import {store} from "./store";
-import {Provider} from "react-redux";
+import { BrowserRouter, HashRouter } from "react-router-dom";
+import { store } from "./store";
+import { Provider } from "react-redux";
 
 export const CalendarApp = () => {
   return (
-    <Provider store={ store }>
-      <BrowserRouter>
+    <Provider store={store}>
+      <HashRouter>
+        {/* <BrowserRouter> */}
         <AppRouter />
-      </BrowserRouter>
+        {/* </BrowserRouter> */}
+      </HashRouter>
     </Provider>
   );
 };
